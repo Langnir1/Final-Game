c8410236d62eddaa6a78fbe007dec60fcba3121d
Langnir1 <joshua.halamka@bsu.edu>
2024-11-25 10:07:22 -0500

    n

--- Defense-game.py
+++ Defense-game.py
@@ -2,5 +2,12 @@ import simpleGE, pygame, random
 
 class Game(simpleGE.Scene):
     def __init__(self):
-        super().__init__(scene)
+        super().__init__()
         self.setImage = "Star-space.jpg"
+
+def main():
+    game = Game()
+    game.start()
+    
+if __name__ == "__main__":
+    main()
