2bc33cdd7edcb46d412d234efd0c436fece1553f
Langnir1 <joshua.halamka@bsu.edu>
2024-12-02 10:51:19 -0500

    Added 2 new classes

--- Defense-game.py
+++ Defense-game.py
@@ -1,18 +1,19 @@
 import simpleGE, pygame, random
 
+class Planet(simpleGE.Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.setImage = ("artificialPlanet.png")
+        self.position = (320, 240)
+        
 class Asteroids(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
-        
-        self.setImage = "Layered Rock.png"
+        self.setImage = ("Layered Rock.png")
         #self.setSize = (30, 30)
         self.reset()
         
     def reset(self):
-       # self.y = 0
-       # self.x = random.randint(0, self.screenWidth)
-       # self.dy = random.randint(3, 5)
-        
         location = ["North", "South" ,"East", "West"]
         result = random.choice(location)
         print(result)
@@ -33,19 +34,34 @@ class Asteroids(simpleGE.Sprite):
             self.y = random.randint(0, self.screenHeight)
             self.x = 0
             self.dx = random.randint(1, 3)
-        
+
+class LabelScore(simpleGE.Label):
+    def __init__(self):
+        super().__init__()
+        self.text = "Score = 0"
+        self.center = (100, 30)
 
 class Game(simpleGE.Scene):
     def __init__(self):
         super().__init__()
-      #  self.setImage = "Star-space.jpg"
+        self.setImage = ("Star_Space.png")
         self.numAsteroid = 10
         self.asteroid = []
         for i in range(self.numAsteroid):
             self.asteroid.append(Asteroids(self))
         
-        self.sprites = [self.asteroid]
+        self.planet = Planet(self)
+        self.labelScore = LabelScore()
         
+        self.sprites = [self.planet,
+                        self.asteroid,
+                        self.labelScore]
+        
+    def process(self):
+        for asteroid in self.asteroid:
+            if self.planet.collidesWith(asteroid):
+                asteroid.reset()
+                
 def main():
     game = Game()
     game.start()
